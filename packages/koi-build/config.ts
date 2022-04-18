ae684636aaa054a1781f6c7658e94c75acae2267
Il Harper <afanyiyu@hotmail.com>
2022-04-18 17:42:32 +0800

    feat: update build config

--- packages/koi-build/config.ts
+++ packages/koi-build/config.ts
@@ -1,3 +1,27 @@
+import { info } from 'gulplog'
+import { spawnOut } from './utils'
+
 export const nodeVersion = '12.22.12'
 
 export const defaultInstance = 'adace8ea4130c619a7376e8e117780102e67dca7'
+
+export const defaultNpmrc = 'registry=https://registry.npmmirror.com/'
+
+export const defaultKoiConfig = `
+mode: portable
+target: ${defaultInstance}
+`.trim()
+
+let koiVersionTemp = ''
+
+export async function getKoiVersion(): Promise<string> {
+  if (koiVersionTemp) return koiVersionTemp
+  try {
+    koiVersionTemp =
+      'v' + (await spawnOut('git', ['describe', '--tags', '--dirty']))
+  } catch (error) {
+    koiVersionTemp = 'v0.0.1'
+  }
+  info(`Use koi version ${koiVersionTemp}`)
+  return koiVersionTemp
+}
