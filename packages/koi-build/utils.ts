f9a6288ec2959fa9b61248d02e09417a5c0d6519
Il Harper <afanyiyu@hotmail.com>
2022-04-18 17:35:26 +0800

    feat: add spawn utils

--- packages/koi-build/utils.ts
+++ packages/koi-build/utils.ts
@@ -1,3 +1,5 @@
+import { SpawnOptions } from 'child_process'
+import { spawn } from 'cross-spawn'
 import * as fs from 'fs'
 
 export async function exists(path: fs.PathLike): Promise<boolean> {
@@ -11,4 +13,40 @@ export async function exists(path: fs.PathLike): Promise<boolean> {
 
 export async function notEmpty(path: fs.PathLike): Promise<boolean> {
   return Boolean((await fs.promises.readdir(path)).length)
+}
+
+export async function spawnOut(
+  command: string,
+  args?: ReadonlyArray<string>,
+  options?: SpawnOptions
+): Promise<string> {
+  const parsedArgs = args ?? []
+  const parsedOptions: SpawnOptions = Object.assign(
+    {},
+    { stdio: 'ignore' },
+    options
+  )
+  const child = spawn(command, parsedArgs, parsedOptions)
+  let stdout = ''
+  child.stdout?.on('data', (x) => (stdout += x))
+  return new Promise<string>((resolve) => {
+    child.on('close', () => resolve(stdout))
+  })
+}
+
+export async function spawnAsync(
+  command: string,
+  args?: ReadonlyArray<string>,
+  options?: SpawnOptions
+): Promise<number> {
+  const parsedArgs = args ?? []
+  const parsedOptions: SpawnOptions = Object.assign(
+    {},
+    { stdio: 'inherit' },
+    options
+  )
+  const child = spawn(command, parsedArgs, parsedOptions)
+  return new Promise<number>((resolve) => {
+    child.on('close', resolve)
+  })
 }
