5fc98bad586877c7d6f15d2030eaef16d187b577
Il Harper <afanyiyu@hotmail.com>
2022-04-20 04:51:58 +0800

    feat: implement default command `run`

--- packages/koi/cli/cli.go
+++ packages/koi/cli/cli.go
@@ -59,12 +59,42 @@ var (
 		CommandNotFound: func(context *cli.Context, s string) {
 			l.Fatalf("Command not found: %s", s)
 		},
-
-		//Action: RunAction,
 	}
+
+	defaultCommand = "run"
 )
 
 func Run(args []string) error {
+	if len(args) <= 1 {
+		args = append(args, defaultCommand)
+		return runIntl(args)
+	}
+
+	var commands []string
+	for _, cmd := range app.Commands {
+		commands = append(commands, cmd.Names()...)
+	}
+
+	for _, x := range args[1:] {
+		for _, y := range commands {
+			if x == y {
+				return runIntl(args)
+			}
+		}
+	}
+
+	/* koi [global options] command [command options]
+	 * Kinda hAcK for now !cuz "run" don't have options
+	 */
+	// newArgs := []string{args[0], defaultCommand}
+	newArgs := []string{args[0]}
+	newArgs = append(newArgs, args[1:]...)
+	newArgs = append(newArgs, defaultCommand)
+
+	return runIntl(newArgs)
+}
+
+func runIntl(args []string) error {
 	l.Debug("Constructing cli app")
 
 	cli.VersionPrinter = func(c *cli.Context) {
