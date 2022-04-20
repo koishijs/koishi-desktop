fc8f0db2ec88d44a31a83fd28cb8dbee34ac7fbe
Il Harper <afanyiyu@hotmail.com>
2022-04-20 16:25:26 +0800

    feat: add `NodeCmd`

--- packages/koi/daemon/node_cmd.go
+++ packages/koi/daemon/node_cmd.go
@@ -1,6 +1,8 @@
 package daemon
 
 import (
+	log "github.com/sirupsen/logrus"
+	"io"
 	"koi/config"
 	"os"
 	"os/exec"
@@ -8,13 +10,23 @@ import (
 	"strings"
 )
 
+var (
+	// Log
+	lKoishi = log.WithField("package", "koishi")
+)
+
+type NodeCmd struct {
+	Cmd    exec.Cmd
+	stderr io.Reader
+}
+
 func CreateNodeCmd(
 	path string,
 	args []string,
 	dir string,
 	useDataHome bool,
 	useDataTemp bool,
-) exec.Cmd {
+) NodeCmd {
 	l.Debug("Getting env.")
 	env := os.Environ()
 
@@ -82,17 +94,57 @@ func CreateNodeCmd(
 	}
 	pathEnv += config.Config.InternalNodeDir
 
-	return exec.Cmd{
+	l.Debug("Now constructing NodeCmd.")
+	errReader, errWriter := io.Pipe()
+	cmd := exec.Cmd{
 		Path:         path,
 		Args:         args,
 		Env:          env,
 		Dir:          dir,
 		Stdin:        nil,
-		Stdout:       nil,
-		Stderr:       nil,
+		Stdout:       os.Stdout,
+		Stderr:       errWriter,
 		ExtraFiles:   nil,
 		SysProcAttr:  nil,
 		Process:      nil,
 		ProcessState: nil,
 	}
+
+	return NodeCmd{
+		Cmd:    cmd,
+		stderr: errReader,
+	}
+}
+
+func (c *NodeCmd) Run() error {
+	l.Debug("Now run NodeCmd.")
+	if err := c.Start(); err != nil {
+		return err
+	}
+	return c.Wait()
+}
+
+func (c *NodeCmd) Start() error {
+	l.Debug("Now start stderr reader.")
+	go func() {
+		p := make([]byte, 1024)
+		for {
+			n, err := c.stderr.Read(p)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				l.Debugf("stderr.Read() err: %s", err)
+			}
+			lKoishi.Info(string(p[:n]))
+		}
+	}()
+
+	l.Debug("Now start NodeCmd.")
+	return c.Cmd.Start()
+}
+
+func (c *NodeCmd) Wait() error {
+	l.Debug("Now wait NodeCmd.")
+	return c.Cmd.Wait()
 }
