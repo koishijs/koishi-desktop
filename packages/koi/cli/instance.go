f8ea1f2b6d70dc9f690690dbbaea198b275358cd
Il Harper <afanyiyu@hotmail.com>
2022-04-20 21:36:16 +0800

    feat: add `create-koishi` options

--- packages/koi/cli/instance.go
+++ packages/koi/cli/instance.go
@@ -31,6 +31,24 @@ var (
 						Aliases: []string{"p"},
 						Usage:   "Install additional packages in instance",
 					},
+
+					&cli.StringFlag{
+						Name:    "ref",
+						Aliases: []string{"r"},
+						Usage:   "The ref of the boilerplate to use",
+					},
+
+					&cli.StringFlag{
+						Name:    "mirror",
+						Aliases: []string{"m"},
+						Usage:   "The GitHub mirror to use",
+					},
+
+					&cli.StringFlag{
+						Name:    "template",
+						Aliases: []string{"t"},
+						Usage:   "The template repo to use",
+					},
 				},
 
 				Action: createInstanceAction,
@@ -59,10 +77,24 @@ func createInstanceAction(c *cli.Context) error {
 		}
 	}
 
+	l.Debug("Constructing args.")
+	args := []string{"init", "koishi", name, "-y"}
+	if ref := strings.Trim(c.String("ref"), " "); ref != "" {
+		args = append(args, "-r", ref)
+	}
+	if mirror := strings.Trim(c.String("mirror"), " "); mirror != "" {
+		args = append(args, "-m", mirror)
+	}
+	if template := strings.Trim(c.String("template"), " "); template != "" {
+		args = append(args, "-t", template)
+	}
+	l.Debug("Constructed. Args:")
+	l.Debug(args)
+
 	l.Debug("Now init koishi.")
 	err := daemon.RunNodeCmd(
 		"npm",
-		[]string{"init", "koishi", name, "-y"},
+		args,
 		config.Config.InternalInstanceDir,
 	)
 	if err != nil {
@@ -73,7 +105,7 @@ func createInstanceAction(c *cli.Context) error {
 	dir := path.Join(config.Config.InternalInstanceDir, name)
 	if len(packages) > 0 {
 		l.Debug("Now install packages.")
-		args := []string{"i", "-S"}
+		args = []string{"i", "-S"}
 		args = append(args, packages...)
 		err = daemon.RunNodeCmd(
 			"npm",
