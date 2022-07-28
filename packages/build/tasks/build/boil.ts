fa52e15450bebfde625d37ddf481aff56f319f83
Il Harper <hi@ilharper.com>
2022-07-28 23:32:25 +0800

    feat: add `boil` task

---
+++ packages/build/tasks/build/boil.ts
@@ -0,0 +1,8 @@
+import { dir } from '../../utils/path'
+import { exec } from '../../utils/spawn'
+
+export const boil = () =>
+  exec(process.platform === 'win32' ? 'koi' : './koi', [
+    'import',
+    dir('buildCache', 'boilerplate.zip'),
+  ])
--- packages/build/tasks/build/index.ts
+++ packages/build/tasks/build/index.ts
@@ -1,8 +1,10 @@
 import { series } from 'gulp'
+import { boil } from './boil'
 import { compile } from './compile'
 import { generate } from './generate'
 
-export * from './generate'
+export * from './boil'
 export * from './compile'
+export * from './generate'
 
-export const build = series(generate, compile)
+export const build = series(generate, compile, boil)
