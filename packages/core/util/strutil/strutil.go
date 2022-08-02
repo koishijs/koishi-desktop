a8d54a0e0182b680407b78f2e1638e8ec54f68ec
Il Harper <hi@ilharper.com>
2022-08-03 05:12:17 +0800

    feat: update `strutil`

--- packages/core/util/strutil/strutil.go
+++ packages/core/util/strutil/strutil.go
@@ -6,8 +6,8 @@ import (
 
 var (
 	trimSet       = string([]rune{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 28, 29, 30, 31, 32, 12288})
-	ColorStartCtr = string([]rune{27, 91})                  // Known as: (1b)[
-	ResetCtrlStr  = ColorStartCtr + string([]rune{48, 109}) // Known as: (1b)[0m
+	ColorStartCtr = string([]rune{27, 91})                  // Known as: (CSI or esc or \033 or \x1b or \e)[
+	ResetCtrlStr  = ColorStartCtr + string([]rune{48, 109}) // Known as: (CSI or esc or \033 or \x1b or \e)[0m
 )
 
 func Trim(s string) string {
