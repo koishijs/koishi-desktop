51731725cf52e2ab6bcb5923c9e82389b0c95a1e
Il Harper <hi@ilharper.com>
2022-08-06 15:55:23 +0800

    refactor: async `WsSender`

--- packages/core/logger/wssender.go
+++ packages/core/logger/wssender.go
@@ -11,18 +11,20 @@ type WsSender struct {
 	c chan rpl.Log
 }
 
-func NewWsSender(localL *rpl.Logger, ws *websocket.Conn) *WsSender {
-	var channel chan rpl.Log
-	go func(l *rpl.Logger, w *websocket.Conn, c chan rpl.Log) {
+func NewWsSender(l *rpl.Logger, ws *websocket.Conn) *WsSender {
+	var c chan rpl.Log
+	go func(l1 *rpl.Logger, w *websocket.Conn, c1 chan rpl.Log) {
 		for {
-			log := <-c
-			err := net.JSON.Send(w, proto.NewLog(log))
-			if err != nil {
-				l.Errorf("failed to send log %v: %s", log, err)
-			}
+			log := <-c1
+			go func(log1 rpl.Log) {
+				err := net.JSON.Send(w, proto.NewLog(log1))
+				if err != nil {
+					l1.Errorf("failed to send log %v: %s", log1, err)
+				}
+			}(log)
 		}
-	}(localL, ws, channel)
-	return &WsSender{c: channel}
+	}(l, ws, c)
+	return &WsSender{c: c}
 }
 
 func (sender *WsSender) Writer() chan<- rpl.Log {
