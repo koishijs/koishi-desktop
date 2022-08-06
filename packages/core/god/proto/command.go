9e6de9b5e318858e7482cd2ece0775a0f61ce7a0
Il Harper <hi@ilharper.com>
2022-08-06 16:17:16 +0800

    feat: add `proto.CommandRequest`

---
+++ packages/core/god/proto/command.go
@@ -0,0 +1,23 @@
+package proto
+
+// CommandRequest is a request of command.
+// Use kebab-case for command name and flags.
+type CommandRequest struct {
+	// Name is the command name of this [proto.CommandRequest] in kebab-case,
+	// like "config" or "dry-run".
+	Name string `json:"name" mapstructure:"name"`
+
+	// Flags is a set of flags, which use kebab-case for keys
+	// and builtin types for values.
+	Flags map[string]any `json:"flags" mapstructure:"flags"`
+}
+
+func NewCommandRequest(name string, flags map[string]any) *Request {
+	return &Request{
+		Type: "cmd",
+		Data: &CommandRequest{
+			Name:  name,
+			Flags: flags,
+		},
+	}
+}
