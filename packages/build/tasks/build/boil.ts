a96fb58b73f2e2f3e365c305854e90f5f166ff5a
Il Harper <hi@ilharper.com>
2022-08-07 22:20:45 +0800

    fix: fix `boil` task

--- packages/build/tasks/build/boil.ts
+++ packages/build/tasks/build/boil.ts
@@ -2,7 +2,8 @@ import { dir } from '../../utils/path'
 import { exec } from '../../utils/spawn'
 
 export const boil = () =>
-  exec(process.platform === 'win32' ? 'koi' : './koi', [
-    'import',
-    dir('buildCache', 'boilerplate.zip'),
-  ])
+  exec(
+    process.platform === 'win32' ? 'koi' : './koi',
+    ['import', dir('buildCache', 'boilerplate.zip')],
+    dir('buildPortable')
+  )
