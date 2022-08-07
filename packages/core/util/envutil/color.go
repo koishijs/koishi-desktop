8afef30b00d9c0ee2d5d88f4205f2c73efe13fbe
Il Harper <hi@ilharper.com>
2022-08-08 04:02:58 +0800

    refactor: migrate `UseColorEnv()`

---
+++ packages/core/util/envutil/color.go
@@ -0,0 +1,9 @@
+package envutil
+
+func UseColorEnv(env *[]string, mode int8) {
+	UseEnv(env, "FORCE_COLOR", "3")
+	UseEnv(env, "COLORTERM", "truecolor")
+	UseEnv(env, "TERM", "xterm-truecolor")
+	UseEnv(env, "CLICOLOR", "1")
+	UseEnv(env, "TERM_PROGRAM", "Koi")
+}
