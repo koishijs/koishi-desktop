cdd2a1e1ee9caf38485eed0447eaf05cff8ea92d
Il Harper <hi@ilharper.com>
2022-08-07 08:25:50 +0800

    feat: add `di.Scope`

---
+++ packages/core/util/di/scope.go
@@ -0,0 +1,16 @@
+package di
+
+import "github.com/samber/do"
+
+func Scope(i *do.Injector) (o *do.Injector) {
+	o = do.New()
+	services := i.ListProvidedServices()
+	for _, service := range services {
+		do.ProvideNamedValue(
+			o,
+			service,
+			do.MustInvokeNamed(i, service),
+		)
+	}
+	return
+}
