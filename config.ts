f82ef020bfa79ee931e3200d0801093f7ff52059
Il Harper <hi@ilharper.com>
2022-08-08 00:34:09 +0800

    feat: update `defaultInstance`

--- config.ts
+++ config.ts
@@ -36,7 +36,7 @@ export const versionBoilerplate = 'v1.0.3'
 /**
  * ID of the default instance.
  */
-export const defaultInstance = 'adace8ea4130c619a7376e8e117780102e67dca7'
+export const defaultInstance = 'default'
 
 //#endregion
 
