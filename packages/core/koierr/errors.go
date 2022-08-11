bdf1c16c36a7c0b5681d1545009579436b653577
Il Harper <hi@ilharper.com>
2022-08-12 02:29:34 +0800

    feat: add `ErrorDict`

--- packages/core/koierr/errors.go
+++ packages/core/koierr/errors.go
@@ -3,4 +3,9 @@ package koierr
 var (
 	ErrSuccess = NewKoiError(0, "success", nil)
 	ErrUnknown = NewKoiError(1, "unknown error", nil)
+
+	ErrorDict = map[uint16]*KoiError{
+		0: ErrSuccess,
+		1: ErrUnknown,
+	}
 )
