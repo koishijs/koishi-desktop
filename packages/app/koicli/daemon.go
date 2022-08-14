237b894ca1a96285e15a558dacd8c2da523abff1
Il Harper <hi@ilharper.com>
2022-08-15 05:30:03 +0800

    feat: implement command daemon stop

--- packages/app/koicli/daemon.go
+++ packages/app/koicli/daemon.go
@@ -5,6 +5,7 @@ import (
 	"github.com/urfave/cli/v2"
 	"gopkg.ilharper.com/koi/core/koiconfig"
 	"gopkg.ilharper.com/koi/core/logger"
+	"gopkg.ilharper.com/koi/sdk/client"
 	"gopkg.ilharper.com/koi/sdk/manage"
 )
 
@@ -12,10 +13,12 @@ const (
 	serviceCommandDaemon = "gopkg.ilharper.com/koi/app/koicli/command.Daemon"
 
 	serviceActionDaemonPing = "gopkg.ilharper.com/koi/app/koicli/action.DaemonPing"
+	serviceActionDaemonStop = "gopkg.ilharper.com/koi/app/koicli/action.DaemonStop"
 )
 
 func newDaemonCommand(i *do.Injector) (*cli.Command, error) {
 	do.ProvideNamed(i, serviceActionDaemonPing, newDaemonPingAction)
+	do.ProvideNamed(i, serviceActionDaemonStop, newDaemonStopAction)
 
 	return &cli.Command{
 		Name:  "daemon",
@@ -25,6 +28,11 @@ func newDaemonCommand(i *do.Injector) (*cli.Command, error) {
 				Name:   "ping",
 				Usage:  "Ping current daemon",
 				Action: do.MustInvokeNamed[cli.ActionFunc](i, serviceActionDaemonPing),
+			},
+			{
+				Name:   "stop",
+				Usage:  "Stop all daemons",
+				Action: do.MustInvokeNamed[cli.ActionFunc](i, serviceActionDaemonStop),
 			},
 		},
 	}, nil
@@ -48,5 +56,33 @@ func newDaemonPingAction(i *do.Injector) (cli.ActionFunc, error) {
 		l.Success("PONG at:\n%#+v", conn)
 
 		return
+	}, nil
+}
+
+func newDaemonStopAction(i *do.Injector) (cli.ActionFunc, error) {
+	l := do.MustInvoke[*logger.Logger](i)
+
+	return func(c *cli.Context) error {
+		var err error
+
+		cfg, err := do.Invoke[*koiconfig.Config](i)
+		if err != nil {
+			return err
+		}
+
+		manager := manage.NewKoiManager(cfg.Computed.Exe, cfg.Computed.DirLock)
+		conn, err := manager.Available()
+		if err != nil {
+			l.Success("No running daemon.")
+			return nil
+		}
+
+		err = client.Stop(conn)
+		if err != nil {
+			return err
+		}
+
+		l.Success("Daemon stopped.")
+		return nil
 	}, nil
 }
