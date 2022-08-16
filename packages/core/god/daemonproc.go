00070ed68e09c35a9f005701a38677c2bd0ade38
Il Harper <hi@ilharper.com>
2022-08-16 09:21:07 +0800

    feat: add `DaemonProc`

---
+++ packages/core/god/daemonproc.go
@@ -0,0 +1,20 @@
+package god
+
+import "github.com/samber/do"
+
+type DaemonProc struct {
+}
+
+func NewDaemonProc(i *do.Injector) (*DaemonProc, error) {
+	return &DaemonProc{}, nil
+}
+
+// Daemon Koishi processes.
+// This will always return an error.
+func (daemonProc *DaemonProc) Daemon() error {
+	panic(0)
+}
+
+func (daemonProc *DaemonProc) Shutdown() error {
+	panic(0)
+}
