e56a615fea2777d27f4e6cc305bdb08eb16d75ca
Il Harper <hi@ilharper.com>
2022-08-19 00:51:00 +0800

    feat: add `ErrNotImplemented`

--- packages/core/koierr/errors.go
+++ packages/core/koierr/errors.go
@@ -1,11 +1,13 @@
 package koierr
 
 var (
-	ErrSuccess = NewKoiError(0, "success", nil)
-	ErrUnknown = NewKoiError(1, "unknown error", nil)
+	ErrSuccess        = NewKoiError(0, "success", nil)
+	ErrUnknown        = NewKoiError(1, "unknown error", nil)
+	ErrNotImplemented = NewKoiError(501, "not implemented", nil)
 
 	ErrorDict = map[uint16]*KoiError{
-		0: ErrSuccess,
-		1: ErrUnknown,
+		0:   ErrSuccess,
+		1:   ErrUnknown,
+		501: ErrNotImplemented,
 	}
 )
