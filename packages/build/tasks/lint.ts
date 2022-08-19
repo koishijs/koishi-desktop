d7e5d5d8b160e0bb04e778337c32c8d99e651e41
Il Harper <hi@ilharper.com>
2022-08-20 00:04:45 +0800

    chore: fix task lint on Windows

--- packages/build/tasks/lint.ts
+++ packages/build/tasks/lint.ts
@@ -3,11 +3,7 @@ import { dir } from '../utils/path'
 import { exec } from '../utils/spawn'
 
 export const lint = () =>
-  eachModule((pkg) =>
-    exec('golangci-lint', ['run', `packages/${pkg}/...`], dir('root'))
-  )
+  eachModule((pkg) => exec('golangci-lint', ['run'], dir('packages', pkg)))
 
 export const lintFix = () =>
-  eachModule((pkg) =>
-    exec('golangci-lint', ['run', `packages/${pkg}/...`, '--fix'], dir('root'))
-  )
+  eachModule((pkg) => exec('golangci-lint', ['run', '--fix'], dir('root', pkg)))
