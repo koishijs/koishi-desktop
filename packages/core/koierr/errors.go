aa5d68710265f87fcec2a6e88670c6367e73929e
Il Harper <hi@ilharper.com>
2022-08-21 04:01:45 +0800

    feat: add `koierr.ErrBadRequest`

--- packages/core/koierr/errors.go
+++ packages/core/koierr/errors.go
@@ -3,11 +3,13 @@ package koierr
 var (
 	ErrSuccess        = NewKoiError(0, "success", nil)
 	ErrUnknown        = NewKoiError(1, "unknown error", nil)
+	ErrBadRequest     = NewKoiError(400, "bad request", nil)
 	ErrNotImplemented = NewKoiError(501, "not implemented", nil)
 
 	ErrorDict = map[uint16]*KoiError{
 		0:   ErrSuccess,
 		1:   ErrUnknown,
+		400: ErrBadRequest,
 		501: ErrNotImplemented,
 	}
 )
