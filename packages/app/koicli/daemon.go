074403e9e360afd9c14352ddd396486d9989ae18
Il Harper <hi@ilharper.com>
2022-08-20 18:11:15 +0800

    feat: add `daemon kill` command

--- packages/app/koicli/daemon.go
+++ packages/app/koicli/daemon.go
@@ -14,11 +14,13 @@ const (
 
 	serviceActionDaemonPing = "gopkg.ilharper.com/koi/app/koicli/action.DaemonPing"
 	serviceActionDaemonStop = "gopkg.ilharper.com/koi/app/koicli/action.DaemonStop"
+	serviceActionDaemonKill = "gopkg.ilharper.com/koi/app/koicli/action.DaemonKill"
 )
 
 func newDaemonCommand(i *do.Injector) (*cli.Command, error) {
 	do.ProvideNamed(i, serviceActionDaemonPing, newDaemonPingAction)
 	do.ProvideNamed(i, serviceActionDaemonStop, newDaemonStopAction)
+	do.ProvideNamed(i, serviceActionDaemonKill, newDaemonKillAction)
 
 	return &cli.Command{
 		Name:  "daemon",
@@ -33,6 +35,11 @@ func newDaemonCommand(i *do.Injector) (*cli.Command, error) {
 				Name:   "stop",
 				Usage:  "Stop all daemons",
 				Action: do.MustInvokeNamed[cli.ActionFunc](i, serviceActionDaemonStop),
+			},
+			{
+				Name:   "kill",
+				Usage:  "Kill all daemons",
+				Action: do.MustInvokeNamed[cli.ActionFunc](i, serviceActionDaemonKill),
 			},
 		},
 	}, nil
@@ -87,6 +94,25 @@ func newDaemonStopAction(i *do.Injector) (cli.ActionFunc, error) {
 		}
 
 		l.Success("Daemon stopped.")
+		return nil
+	}, nil
+}
+
+func newDaemonKillAction(i *do.Injector) (cli.ActionFunc, error) {
+	l := do.MustInvoke[*logger.Logger](i)
+
+	return func(c *cli.Context) error {
+		l.Debug("Trigger action: daemon kill")
+
+		cfg, err := do.Invoke[*koiconfig.Config](i)
+		if err != nil {
+			return err
+		}
+
+		killed := manage.NewKoiManager(cfg.Computed.Exe, cfg.Computed.DirLock).Kill()
+
+		l.Successf("%d Daemon killed.", killed)
+
 		return nil
 	}, nil
 }
