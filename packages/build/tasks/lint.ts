2d47c1781b21ec66310b92fc625014ffe281aab9
Il Harper <hi@ilharper.com>
2022-08-20 17:49:13 +0800

    fix: fix lint task

--- packages/build/tasks/lint.ts
+++ packages/build/tasks/lint.ts
@@ -1,11 +1,11 @@
-import { eachModule } from '../utils/module'
+import { tryEachModule } from '../utils/module'
 import { dir } from '../utils/path'
 import { exec } from '../utils/spawn'
 
 export const lint = () =>
-  eachModule((pkg) => exec('golangci-lint', ['run'], dir('packages', pkg)))
+  tryEachModule((pkg) => exec('golangci-lint', ['run'], dir('packages', pkg)))
 
 export const lintFix = () =>
-  eachModule((pkg) =>
+  tryEachModule((pkg) =>
     exec('golangci-lint', ['run', '--fix'], dir('packages', pkg))
   )
