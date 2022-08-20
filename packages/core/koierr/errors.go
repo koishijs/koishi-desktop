a20e0243dd15ef1442d3ec220c85c34213b222a4
Il Harper <hi@ilharper.com>
2022-08-21 04:39:11 +0800

    feat: update errors

--- packages/core/koierr/errors.go
+++ packages/core/koierr/errors.go
@@ -1,20 +1,27 @@
 package koierr
 
+import "fmt"
+
 var (
 	NewErrSuccess        = func(err error) *KoiError { return NewKoiError(0, "success", err) }
 	NewErrUnknown        = func(err error) *KoiError { return NewKoiError(1, "unknown error", err) }
 	NewErrBadRequest     = func(err error) *KoiError { return NewKoiError(400, "bad request", err) }
+	NewErrInternalError  = func(err error) *KoiError { return NewKoiError(500, "internal error", err) }
 	NewErrNotImplemented = func(err error) *KoiError { return NewKoiError(501, "not implemented", err) }
 
 	ErrSuccess        = NewErrSuccess(nil)
 	ErrUnknown        = NewErrUnknown(nil)
 	ErrBadRequest     = NewErrBadRequest(nil)
+	ErrInternalError  = NewErrInternalError(nil)
 	ErrNotImplemented = NewErrNotImplemented(nil)
+
+	NewErrInstanceExists = func(name string) *KoiError { return NewKoiError(1100, fmt.Sprintf("instance %s already exists", name), nil) }
 
 	ErrorDict = map[uint16]*KoiError{
 		0:   ErrSuccess,
 		1:   ErrUnknown,
 		400: ErrBadRequest,
+		500: ErrInternalError,
 		501: ErrNotImplemented,
 	}
 )
