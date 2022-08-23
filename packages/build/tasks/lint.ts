9b513926ee3585e9d411020a76261a2fc12ed2bd
Il Harper <hi@ilharper.com>
2022-08-23 21:47:40 +0800

    feat: update linter args

--- packages/build/tasks/lint.ts
+++ packages/build/tasks/lint.ts
@@ -2,10 +2,22 @@ import { tryEachModule } from '../utils/module'
 import { dir } from '../utils/path'
 import { exec } from '../utils/spawn'
 
+const buildLintArgs = (pkg: string, fix?: boolean) => {
+  const isGh = process.env.GITHUB_ACTION
+  const args = ['run']
+  if (isGh) args.push('--out-format=github-actions')
+  else args.push('--out-format=colored-line-number')
+  args.push(`--path-prefix=${dir('packages', pkg)}`)
+  if (fix) args.push('--fix')
+  return args
+}
+
 export const lint = () =>
-  tryEachModule((pkg) => exec('golangci-lint', ['run'], dir('packages', pkg)))
+  tryEachModule((pkg) =>
+    exec('golangci-lint', buildLintArgs(pkg), dir('packages', pkg))
+  )
 
 export const lintFix = () =>
   tryEachModule((pkg) =>
-    exec('golangci-lint', ['run', '--fix'], dir('packages', pkg))
+    exec('golangci-lint', buildLintArgs(pkg, true), dir('packages', pkg))
   )
