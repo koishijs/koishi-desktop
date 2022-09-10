4724e788e1e8c8b7b5c7dd3610185063577e82ea
Il Harper <hi@ilharper.com>
2022-09-10 21:10:00 +0800

    feat: lock `ChannelRegistry`

--- packages/app/ui/tray/chanreg.go
+++ packages/app/ui/tray/chanreg.go
@@ -1,10 +1,15 @@
 package tray
 
-import "github.com/samber/do"
+import (
+	"sync"
+
+	"github.com/samber/do"
+)
 
 const serviceTrayChannelRegistry = "gopkg.ilharper.com/koi/app/ui/tray.ChannelRegistry"
 
 type ChannelRegistry struct {
+	lock  sync.Mutex
 	reg   [256]chan struct{}
 	index uint8
 }
@@ -14,6 +19,9 @@ func NewChannelRegistry(i *do.Injector) (*ChannelRegistry, error) {
 }
 
 func (cr *ChannelRegistry) Insert(ch chan struct{}) {
+	cr.lock.Lock()
+	defer cr.lock.Unlock()
+
 	if cr.reg[cr.index] != nil {
 		close(cr.reg[cr.index])
 	}
