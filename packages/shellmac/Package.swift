171f12ff332fc7966ee80fdd0e9799ea53ef5ad5
Il Harper <hi@ilharper.com>
2022-10-06 17:47:34 +0800

    chore: init `shellmac` package

---
+++ packages/shellmac/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.6
+import PackageDescription
+
+let package = Package(
+    name: "KoiShell",
+    platforms: [
+        .macOS(.v11)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kylehickinson/SwiftUI-WebView", .upToNextMinor(from: "0.3.0")),
+    ],
+    targets: [
+        .executableTarget(
+            name: "KoiShell",
+            dependencies: [
+                .product(name: "WebView", package: "SwiftUI-WebView"),
+            ]),
+    ]
+)
