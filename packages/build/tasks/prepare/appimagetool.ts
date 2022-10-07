b430d1983f5ca7e56ed8dfa609b24b15c8f63369
Il Harper <hi@ilharper.com>
2022-10-08 01:24:09 +0800

    refactor: use `sourceGitHub`

--- packages/build/tasks/prepare/appimagetool.ts
+++ packages/build/tasks/prepare/appimagetool.ts
@@ -1,10 +1,10 @@
 import { info } from 'gulplog'
-import { versionAppimagetool } from '../../utils/config'
+import { sourceGitHub, versionAppimagetool } from '../../utils/config'
 import { exists } from '../../utils/fs'
 import { download } from '../../utils/net'
 import { dir } from '../../utils/path'
 
-const src = `https://github.com/AppImage/AppImageKit/releases/download/${versionAppimagetool}/appimagetool-x86_64.AppImage`
+const src = `${sourceGitHub}/AppImage/AppImageKit/releases/download/${versionAppimagetool}/appimagetool-x86_64.AppImage`
 const destFile = 'appimagetool.AppImage'
 
 export const prepareAppimagetoolDownload = async () => {
