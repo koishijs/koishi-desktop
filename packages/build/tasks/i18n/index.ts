eed238f48ded34f17eeffdfe16baf0336e15a7bc
Il Harper <hi@ilharper.com>
2022-10-17 02:16:43 +0800

    fix(build): fix i18n tasks

--- packages/build/tasks/i18n/index.ts
+++ packages/build/tasks/i18n/index.ts
@@ -1,6 +1,7 @@
 import del from 'del'
+import fs from 'node:fs/promises'
 import { dir } from '../../utils/path'
-import { exec } from '../../utils/spawn'
+import { exec, tryExec } from '../../utils/spawn'
 
 export const i18nExtract = async () => {
   await del(dir('locales'))
@@ -10,4 +11,23 @@ export const i18nExtract = async () => {
     ['extract', '--lang=en-US,zh-CN', 'gopkg.ilharper.com/...'],
     dir('root')
   )
+}
+
+export const i18nGenerate = async () => {
+  await tryExec(
+    'gotext',
+    [
+      '--srclang=en-US',
+      'update',
+      '--lang=en-US,zh-CN',
+      '--out=packages/app/catalog.go',
+      'gopkg.ilharper.com/...',
+    ],
+    dir('root')
+  )
+
+  const pathCatalog = dir('src', 'catalog.go')
+  const catalog = (await fs.readFile(pathCatalog)).toString()
+  catalog.replace('package ', 'package main')
+  await fs.writeFile(pathCatalog, catalog)
 }
