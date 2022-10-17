1a03d19a50f65771cdf083cdc796428cc3699b92
Il Harper <hi@ilharper.com>
2022-10-18 04:35:05 +0800

    feat(shellwin): update

---
+++ packages/shellwin/include/koishell/main.hpp
@@ -0,0 +1,11 @@
+#ifndef _KOISHELL_MAIN_
+#define _KOISHELL_MAIN_
+
+#include <windows.h>
+
+#include "shellcomm/logger.hpp"
+
+int WINAPI wWinMain(
+    HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow);
+
+#endif /* _KOISHELL_MAIN_ */
--- packages/shellwin/src/main.cpp
+++ packages/shellwin/src/main.cpp
@@ -1,1 +1,15 @@
-#include <windows.h>
+#include "koishell/main.hpp"
+
+int WINAPI wWinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    PWSTR pCmdLine,
+    int nCmdShow) {
+  int argc;
+  wchar_t **argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+  if (argc != 2) {
+    shellcomm::log("argc not valid.");
+    return 1;
+  }
+}
