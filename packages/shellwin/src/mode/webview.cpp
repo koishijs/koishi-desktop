5550abdc3fb1368131086497da83bb65c6c9ad94
Il Harper <hi@ilharper.com>
2022-11-04 02:20:44 +0800

    feat(shellwin): implement `WndProc()`

--- packages/shellwin/src/mode/webview.cpp
+++ packages/shellwin/src/mode/webview.cpp
@@ -63,9 +63,14 @@ int WebViewWindow::Run() {
 LRESULT CALLBACK WebViewWindow::WndProc(
     _In_ HWND hWnd, _In_ UINT message, _In_ WPARAM wParam, _In_ LPARAM lParam) {
   switch (message) {
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
   default:
     return DefWindowProcW(hWnd, message, wParam, lParam);
   }
+
+  return 0;
 }
 
 int RunWebView(_In_ HINSTANCE hInstance, _In_ int nCmdShow, _In_ json arg) {
