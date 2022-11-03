f2093e2cb0d6b299c1b1f286fff78a0581a75fbf
Il Harper <hi@ilharper.com>
2022-11-04 02:11:35 +0800

    feat(shellwin): add class members

--- packages/shellwin/include/koishell/mode/webview.hpp
+++ packages/shellwin/include/koishell/mode/webview.hpp
@@ -1,8 +1,11 @@
 #ifndef _KOISHELL_MODE_WEBVIEW_
 #define _KOISHELL_MODE_WEBVIEW_
 
+#include <wil/com.h>
 #include <windows.h>
+#include <wrl.h>
 
+#include "WebView2.h"
 #include "nlohmann/json.hpp"
 
 #include "koishell/util/logger.hpp"
@@ -17,6 +20,8 @@ class WebViewWindow {
   json arg;
 
   WNDCLASSEXW wcex;
+  wil::com_ptr<ICoreWebView2Controller> webviewController;
+  wil::com_ptr<ICoreWebView2> webview;
 
 public:
   WebViewWindow(_In_ HINSTANCE hInstance, _In_ int nCmdShow, _In_ json arg);
