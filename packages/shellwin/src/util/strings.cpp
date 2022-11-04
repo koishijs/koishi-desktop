936055b1cbc8ed1ac12ec5eb6f2f5b26161d2bac
Il Harper <hi@ilharper.com>
2022-11-05 00:32:04 +0800

    fix(shellwin): fix `WideCharToUTF8()` and `UTF8ToWideChar()`

--- packages/shellwin/src/util/strings.cpp
+++ packages/shellwin/src/util/strings.cpp
@@ -9,7 +9,7 @@ char *WideCharToUTF8(_In_ wchar_t *w) {
   char *s = new char[len + 1];
   s[len] = 0;
   WideCharToMultiByte(
-      CP_UTF8, WC_ERR_INVALID_CHARS, w, -1, result, len, nullptr, nullptr);
+      CP_UTF8, WC_ERR_INVALID_CHARS, w, -1, s, len, nullptr, nullptr);
   return s;
 }
 
@@ -19,7 +19,7 @@ wchar_t *UTF8ToWideChar(_In_ char *s) {
   if (!len) return nullptr;
   wchar_t *w = new wchar_t[len + 1];
   w[len] = 0;
-  MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, s, -1, result, len);
+  MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, s, -1, w, len);
   return w;
 }
 
