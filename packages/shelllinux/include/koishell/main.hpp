ff36eab5d75567608fae046d3c89868beb999b6b
Il Harper <hi@ilharper.com>
2022-11-05 00:59:15 +0800

    feat(shelllinux): implement shelllinux

---
+++ packages/shelllinux/include/koishell/main.hpp
@@ -0,0 +1,8 @@
+#ifndef _KOISHELL_MAIN_
+#define _KOISHELL_MAIN_
+
+#include "shellcomm/logger.hpp"
+
+int main(int argc, char *argv[]);
+
+#endif /* _KOISHELL_MAIN_ */
---
+++ packages/shelllinux/src/main.cpp
@@ -0,0 +1,6 @@
+#include "koishell/main.hpp"
+
+int main(int argc, char *argv[]) {
+  ShellComm::Log("Not supported yet.");
+  return 1;
+}
