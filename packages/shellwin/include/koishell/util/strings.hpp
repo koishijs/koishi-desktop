3dd9f0df821029d52de9db377fcf97490f4d682c
Il Harper <hi@ilharper.com>
2022-11-04 23:05:49 +0800

    feat(shellwin): implement `UTF8ToWideChar()`

--- packages/shellwin/include/koishell/util/strings.hpp
+++ packages/shellwin/include/koishell/util/strings.hpp
@@ -7,6 +7,8 @@ namespace KoiShell {
 
 char *WideCharToUTF8(wchar_t *w);
 
+wchar_t *UTF8ToWideChar(_In_ char *s);
+
 } // namespace KoiShell
 
 #endif /* _KOISHELL_STRINGS_ */
--- packages/shellwin/src/util/strings.cpp
+++ packages/shellwin/src/util/strings.cpp
@@ -12,4 +12,14 @@ char *WideCharToUTF8(wchar_t *w) {
   return result;
 }
 
+wchar_t *UTF8ToWideChar(_In_ char *s) {
+  int len =
+      MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, s, -1, nullptr, 0);
+  if (!len) return nullptr;
+  wchar_t *w = new wchar_t[len + 1];
+  w[len] = 0;
+  MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, s, -1, result, len);
+  return w;
+}
+
 } // namespace KoiShell
