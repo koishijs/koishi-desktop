bcd4fdbead45778df50f8403b30bcfba8e694114
Il Harper <hi@ilharper.com>
2022-11-07 03:08:51 +0800

    chore: bump boilerplate to `v1.2.3`

--- config.ts
+++ config.ts
@@ -38,7 +38,7 @@ export const versionToolsWix = '311'
 // These are defaults for koishi-desktop.
 
 export const repoBoilerplate = 'koishijs/boilerplate'
-export const versionBoilerplate = 'v1.2.2'
+export const versionBoilerplate = 'v1.2.3'
 
 /**
  * ID of the default instance.
