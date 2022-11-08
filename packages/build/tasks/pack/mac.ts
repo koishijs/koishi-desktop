10a37c5d3b4ec85a7d24282267ca692e3d92baeb
Il Harper <hi@ilharper.com>
2022-11-08 23:41:44 +0800

    fix(build): `chown` on mac

--- packages/build/tasks/pack/mac.ts
+++ packages/build/tasks/pack/mac.ts
@@ -59,6 +59,8 @@ echo "Setting chmod for app..."
 sudo chmod -R 755 /Applications/Koishi.app/ || true
 echo "Setting chmod for user data..."
 sudo chmod -R 777 ~/Library/Application\\ Support/Il\\ Harper/Koishi/ || true
+echo "Setting chown for user data..."
+sudo chown -R \${USER}:staff ~/Library/Application\\ Support/Il\\ Harper/Koishi/ || true
 echo "Post-install process finished."
 `.trim()
   )
