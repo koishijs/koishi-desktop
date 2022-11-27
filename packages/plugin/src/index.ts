7c37bc41721dcd34aff7f9bd59949083b4ef2ba2
Il Harper <hi@ilharper.com>
2022-11-28 00:19:30 +0800

    feat(plugin): implement config schema

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -2,9 +2,25 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'desktop'
 
-export interface Config {}
+export interface Config {
+  enhance: boolean
+  telemetry: boolean
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+    enhance: Schema.boolean()
+      .default(true)
+      .description('启用 Koishi 桌面增强。'),
+  }).description('增强'),
+  Schema.object({
+    telemetry: Schema.boolean()
+      .default(true)
+      .description(
+        '启用此实例的 Koishi 桌面遥测。还需在 Koishi 桌面配置中启用遥测。'
+      ),
+  }).description('遥测'),
+])
 
 export function apply(ctx: Context) {
   // write your plugin here
