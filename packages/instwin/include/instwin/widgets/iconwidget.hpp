57660e07b8f0386ec18f4076f353cd48158cf250
Il Harper <hi@ilharper.com>
2023-08-13 00:02:55 +0800

    feat(instwin): extract icon to `IconWidget`

---
+++ packages/instwin/include/instwin/widgets/iconwidget.hpp
@@ -0,0 +1,27 @@
+#ifndef _INSTWIN_WIDGETS_ICONWIDGET_
+#define _INSTWIN_WIDGETS_ICONWIDGET_
+
+#include "instwin/stdafx.hpp"
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class IconWidget;
+}
+QT_END_NAMESPACE
+
+class IconWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit IconWidget(QWidget *parent = nullptr);
+    ~IconWidget();
+
+private:
+    Ui::IconWidget *ui;
+};
+
+
+#endif /* _INSTWIN_WIDGETS_ICONWIDGET_ */
---
+++ packages/instwin/src/widgets/iconwidget.cpp
@@ -0,0 +1,14 @@
+#include "instwin/widgets/iconwidget.hpp"
+#include "ui_iconwidget.h"
+
+IconWidget::IconWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::IconWidget)
+{
+    ui->setupUi(this);
+}
+
+IconWidget::~IconWidget()
+{
+    delete ui;
+}
