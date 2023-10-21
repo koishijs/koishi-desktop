bb8fee2facb20eccca02b6f1a7da7e59e5c8715d
Il Harper <hi@ilharper.com>
2023-10-21 23:40:56 +0800

    feat(core): fix typo

--- packages/core/koishell/koishell.go
+++ packages/core/koishell/koishell.go
@@ -249,7 +249,7 @@ func (shell *KoiShell) About() {
 
 	_, err := shell.exec(map[string]any{
 		"mode":        "dialog",
-		"title":       "about",
+		"title":       "About",
 		"style":       "info",
 		"text1":       "Koishi Desktop",
 		"text2":       fmt.Sprintf("v%s", util.AppVersion),
