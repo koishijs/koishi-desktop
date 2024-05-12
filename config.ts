ec4b9a35ba4313f908f2a41819617312a4482852
Il Harper <hi@ilharper.com>
2024-05-12 22:55:25 +0800

    fix(build): revert "chore: bump gotext version to `v0.15.0`"
    This reverts commit b651883a48f1bbc683c6aedd44bff4137b55be60.
    
    There's another bug in https://github.com/golang/text/commit/f3e69ed4a8ab60c16ae76f4ddb08f2726b0a9428

--- config.ts
+++ config.ts
@@ -28,7 +28,7 @@ export const versionAppimagetool = '13'
 
 export const versionToolsVersioninfo = 'v1.4.0'
 export const versionToolsGolangCILint = 'v1.52.2'
-export const versionToolsGoText = 'v0.15.0'
+export const versionToolsGoText = 'v0.11.0'
 export const versionToolsRcedit = 'v1.1.1'
 export const versionToolsWix = '311'
 
